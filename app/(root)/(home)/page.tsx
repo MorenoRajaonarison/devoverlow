f0338b0546507a707f2e96001eedf8ddbfde0894
MorenoRajaonarison <rajaonarison.moreno@gmail.com>
2024-02-10 11:54:27 +0300

    feat: setup reusable NoResult component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,52 @@
 import Filter from "@/components/shared/Filter/Filter";
 import HomeFilter from "@/components/shared/Filter/HomeFilter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: 1,
+    title: "Quelle est la capitale de la France ?",
+    author: "Jean",
+    upVotes: 10,
+    views: 50,
+    answer: 3,
+    createdAt: new Date("2023-05-15"),
+    tags: [
+      { _id: 1, name: "géographie" },
+      { _id: 2, name: "culture" },
+    ],
+  },
+  {
+    _id: 2,
+    title: "Qu'est-ce que la photosynthèse ?",
+    author: "Marie",
+    upVotes: 20,
+    views: 100,
+    answer: 5,
+    createdAt: new Date("2023-08-20"),
+    tags: [
+      { _id: 3, name: "biologie" },
+      { _id: 4, name: "sciences" },
+    ],
+  },
+  {
+    _id: 3,
+    title: "Comment fonctionne la blockchain ?",
+    author: "Pierre",
+    upVotes: 15,
+    views: 80,
+    answer: 7,
+    createdAt: new Date("2023-10-10"),
+    tags: [
+      { _id: 5, name: "technologie" },
+      { _id: 6, name: "informatique" },
+    ],
+  },
+];
 
 export default function Home() {
   return (
@@ -30,7 +73,22 @@ export default function Home() {
           containerClasses="hidden max-md:flex"
         />
       </div>
+
       <HomeFilter />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((qst) => "Question")
+        ) : (
+          <NoResult
+            title="THere's no question to show"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo,
+          assumenda quasi? Quam recusandae natus incidunt."
+            textBtn="Ask a Question"
+            link="/ask-question"
+          />
+        )}
+      </div>
     </>
   );
 }
